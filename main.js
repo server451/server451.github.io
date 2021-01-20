6311c5e8f4bc0bec4b285061c110b7f5d70acf72
server451 <77158206+server451@users.noreply.github.com>
2021-01-20 17:34:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(function () {
+	$('.buy_item').click(function () {
+		$('#modal').addClass('show');
+    });
+    $('.closeWindow').click(function() {
+		$('#modal').removeClass('show')
+	})
+})
+
+$(function () {
+	$('.buy').click(function () {
+		$('#modal').addClass('show');
+    });
+    $('.closeWindow').click(function() {
+		$('#modal').removeClass('show')
+	})
+})
